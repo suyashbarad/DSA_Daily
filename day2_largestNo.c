72148851cf1c91f48c96fca85deae7bd28a0418a
Suyash Sachin Barad <baradsuyash4@gmail.com>
2025-11-25 21:40:59 +0530

    Fixed logic and updated Day 2 - Largest Number program

--- day2_largestNo.c
+++ day2_largestNo.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
 
 int main(){
-    int n = 4;
-    int arr[4] = {1, 4, 6, 2};
-    int Largest;
+    int n;
+    printf("Enter no. of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+    int Largest = arr[0];
     for(int i = 0; i <= n; i++){
         Largest = i;
         for(int j = 0; j <= n; j++){
