384e107e54f03d1799ff6f32e33712a616e40fc1
suyashh <baradsuyash8@gmail.com>
2025-11-26 15:35:38 +0530

    Refactor palindrome function for strings and numbers
    It can now take strings as well as numbers as input!

--- day3_palindrome.c
+++ day3_palindrome.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
+#include<string.h>
 
-void palindrome(int n, int arr[n]){
+void palindromeString(char str[]){
     int flag = 1;
+    int n = strlen(str);
     for(int i = 0; i < n/2; i++){
-        if(arr[i] != arr[n-i-1]){
+        if(str[i] != str[n-i-1]){
             flag = 0;
             break;
         }
@@ -11,17 +13,43 @@ void palindrome(int n, int arr[n]){
     if(flag) printf("Palindrome\n");
     else printf("Not palindrome\n");
 }
+void palindromeNumber(int n, int arr[]){ 
+    int flag = 1; 
+    for(int i = 0; i < n/2; i++){
+        if(arr[i] != arr[n-i-1]){ 
+            flag = 0; 
+            break; 
+        }
+    } 
+    if(flag) printf("Palindrome\n");
+    else printf("Not palindrome\n"); }
 int main(){
     int n;
-    printf("Enter no. of elements: ");
-    scanf("%d", &n);
-    int arr[n];
-    for(int i = 0; i < n; i++){
-        printf("Enter element %d: ", i+1);
-        scanf("%d", &arr[i]);
+    char str[100];
+    int choice;
+    
+    printf("1.Number or 2.String: ");
+    scanf("%d", &choice);
+    if(choice == 1){
+        printf("Enter no. of elements: ");
+        scanf("%d", &n);
+        int arxr[n];
+        for(int i = 0; i < n; i++){
+            printf("Enter element %d: ", i+1);
+            scanf("%d", &arr[i]);
+        }
+        printf("\n");
+        palindromeNumber(n, arr); 
     }
-    printf("\n");
-
-    palindrome(n, arr);  
+    else if(choice == 2){
+        printf("Enter string: ");
+        scanf("%s", str);
+        n = strlen(str);
+        palindromeString(str);
+    }
+    else{
+        printf("Invalid input! ");
+    }
+     
     return 0;
 }
