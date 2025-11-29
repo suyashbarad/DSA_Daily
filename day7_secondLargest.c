cd53ea9d3145344f92c6617cb63d7c5ad5cdee46
suyashh <baradsuyash8@gmail.com>
2025-11-29 15:06:11 +0530

    Finding the second largest element
    Modified the program to accept user input for the number of elements and the array values instead of using hardcoded values.

--- day7_secondLargest.c
+++ day7_secondLargest.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 
 int main(){
-    int n = 5;
-    int arr[5] = {2, 4, 6, 1, 3};
+    // int n = 5;       //if done manually
+    // int arr[5] = {2, 4, 6, 1, 3};
+
+    int n;
+    printf("Enter no. of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
     
     for(int i = 0; i < n-1; i++){
         int smallest = i;
