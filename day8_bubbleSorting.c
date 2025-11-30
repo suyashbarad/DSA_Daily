210f6944e821c45d56e37de66a93c408e45287f2
suyashh <baradsuyash8@gmail.com>
2025-11-30 14:43:32 +0530

    Day8: Bubble sort
    Modified the bubble sort program to accept user input for the number of elements and the array elements instead of using hardcoded values.

--- day8_bubbleSorting.c
+++ day8_bubbleSorting.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
 
 int main(){
-    int n = 5;
-    int arr[5] = {3, 2, 5, 4, 1};
+    // int n = 5;      //manually
+    // int arr[5] = {3, 2, 5, 4, 1};
+    int n;
+    printf("Enter no. of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
+    
     for(int i = 0; i < n-1; i++){
         for(int j = 0; j < n-1-i; j++){
             if(arr[j] > arr[j+1]){
