ab1c111cbc68a08b51fd45bb7d544ea9bdcc30ed
suyashh <baradsuyash8@gmail.com>
2025-11-30 14:46:34 +0530

    Day8: Bubble sort
    Sorting array using bubble sort

--- day8_bubbleSorting.c
+++ day8_bubbleSorting.c
@@ -12,7 +12,7 @@ int main(){
         scanf("%d", &arr[i]);
     }
     printf("\n");
-    
+
     for(int i = 0; i < n-1; i++){
         for(int j = 0; j < n-1-i; j++){
             if(arr[j] > arr[j+1]){
@@ -22,6 +22,7 @@ int main(){
             }
         }
     }
+    printf("Sorted array using Bubble sort: ");
     for(int i = 0; i < n; i++){
         printf("%d ", arr[i]);
     }
