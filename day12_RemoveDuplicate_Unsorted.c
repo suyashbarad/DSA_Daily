99c0b216c78a41bc132486b2c81f6d5041010dfa
suyashh <baradsuyash8@gmail.com>
2025-12-04 16:56:10 +0530

    Sorting first

--- day12_RemoveDuplicate_Unsorted.c
+++ day12_RemoveDuplicate_Unsorted.c
@@ -13,5 +13,22 @@ int main(){
         printf("Enter element %d: ", i+1);
         scanf("%d", &arr[i]);
     }
+
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("Sorted array: ");
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    
     return 0;
 }
