1237874393e767cbfc0d35929934117d49e8978d
suyashh <baradsuyash8@gmail.com>
2025-12-05 12:15:55 +0530

    Main code

--- day13_secondLargest_shorter.c
+++ day13_secondLargest_shorter.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
-
+//Second largest number
 int main(){
-    // int n = 5;       //if done manually
-    // int arr[5] = {2, 4, 6, 1, 3};
 
-    int n;
-    printf("Enter no. of elements: ");
-    scanf("%d", &n);
-    int arr[n];
-    for(int i = 0; i < n; i++){
-        printf("Enter element %d: ", i+1);
-        scanf("%d", &arr[i]);
-    }
-    printf("\n");
-    int largest;
+    int n = 5;
+    int arr[5] = {3, 2, 1, 5, 4};
+    int largest = arr[0];
+    int largest_index = 0;
     for(int i = 1; i < n; i++){
         if(arr[i] > largest){
             largest = arr[i];
             printf("Largest location: arr[%d] = %d\n", i, arr[i]);
+            largest_index = i;
+        }
+    }
+    int second_largest = arr[0];
+    for(int i = 1; i < n; i++){
+        if(i == largest_index) continue;
+        else{
+            if(arr[i] > second_largest){
+                second_largest = arr[i];
+                printf("Second largest: %d", second_largest);
+            }
         }
     }
     return 0;
