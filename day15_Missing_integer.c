9f5fc2a76af12fe680c85a08661e0ef1be5a8426
suyashh <baradsuyash8@gmail.com>
2025-12-07 00:19:22 +0530

    Finding Missing number between 1 to N

--- day15_Missing_integer.c
+++ day15_Missing_integer.c
@@ -5,11 +5,17 @@ int main(){
     int n;
     printf("Enter N (total numbers from 1 to N): ");
     scanf("%d", &n);
-    int arr[n];
-    for(int i = 0; i < n; i++){
+    int arr[n-1];
+    int total = (n*(n+1))/2;
+    int sum = 0;
+    for(int i = 0; i < n-1; i++){
         //note: Enter elements between 1 to N
         printf("Enter element %d: ", i+1);
         scanf("%d", &arr[i]);
+        sum += arr[i];
     }
-return 0;
+    int missing = total-sum;
+    printf("Missing Integer: %d", missing);
+
+    return 0;
 }
