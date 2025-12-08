df940f720517cacd32d6d4aafd936dc660e7cad5
Suyash Sachin Barad <baradsuyash4@gmail.com>
2025-12-08 23:57:57 +0530

    Day16: Binary Search Tree

--- day15_Missing_integer.c
+++ day15_Missing_integer.c
@@ -5,17 +5,11 @@ int main(){
     int n;
     printf("Enter N (total numbers from 1 to N): ");
     scanf("%d", &n);
-    int arr[n-1];
-    int total = (n*(n+1))/2;
-    int sum = 0;
-    for(int i = 0; i < n-1; i++){
+    int arr[n];
+    for(int i = 0; i < n; i++){
         //note: Enter elements between 1 to N
         printf("Enter element %d: ", i+1);
         scanf("%d", &arr[i]);
-        sum += arr[i];
     }
-    int missing = total-sum;
-    printf("Missing Integer: %d", missing);
-
-    return 0;
+return 0;
 }
