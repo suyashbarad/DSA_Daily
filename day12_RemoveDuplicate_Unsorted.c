28356c65ad4ef48e2a2dd124d364df6e121186fd
suyashh <baradsuyash8@gmail.com>
2025-12-09 00:44:29 +0530

    Finding the Missing Integer

--- day12_RemoveDuplicate_Unsorted.c
+++ day12_RemoveDuplicate_Unsorted.c
@@ -2,7 +2,7 @@
 
 int main(){
     
-    // int arr[] = {1, 2, 1, 1, 2};              //if done in manual way
+    // int arr[] = {1, 2, 1, 1, 2};              //if done in manually
     // int n = sizeof(arr) / sizeof(arr[0]);
     int n;
     printf("Enter no. of elements: ");
