4ac6d575e66e95696b3a1f9f47f31f2cc81eff7d
suyashh <baradsuyash8@gmail.com>
2025-12-10 20:30:10 +0530

    Finding the Missing Integer

--- day15_Missing_integer.c
+++ day15_Missing_integer.c
@@ -13,7 +13,7 @@ int main(){
     int total = (n*(n+1))/2;
     int sum = 0;
     for(int i = 0; i < n-1; i++){
-        //note: Enter elements between 1 to N
+        //note: Enter elements between 1 to N only
         printf("Enter element %d: ", i+1);
         scanf("%d", &arr[i]);
         sum += arr[i];
@@ -28,3 +28,4 @@ int main(){
 
     return 0;
 }
+
