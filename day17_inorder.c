e5153b7b9f9dcfc644ee5726e83aed8f1f202298
suyashh <baradsuyash8@gmail.com>
2025-12-10 20:34:25 +0530

    Inorder recursive and non recursive functions

--- day17_inorder.c
+++ day17_inorder.c
@@ -27,3 +27,34 @@ struct node* insert(struct node* root, int value) {
 
     return root;
 }
+void inorder_rec(struct node* root){
+    if(root != NULL){
+        inorder_rec(root->left);
+        printf("%d ", root->data);
+        inorder_rec(root->right);
+    }
+}
+struct node* stack[100];
+int top = -1;
+void push(struct node* x){
+    stack[++top] = x;
+}
+struct node* pop(){
+    return stack[top--];
+}
+int isEmpty(){
+    return top == -1;
+}
+void inorder_Nonrec(struct node* root){
+    struct node* current = root;
+    while(current != NULL || !isEmpty()){
+        while(current != NULL){
+            push(current);
+            current = current->left;
+        }
+        current = pop();
+        printf("%d ", current->data);
+
+        current = current->right;
+    }
+}
