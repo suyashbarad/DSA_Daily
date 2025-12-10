b4e5a8b4e0159a996e9c783bd1f6bc092c752332
suyashh <baradsuyash8@gmail.com>
2025-12-10 20:33:16 +0530

    creating and inserting node

--- day17_inorder.c
+++ day17_inorder.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-//day19_postorder.c
+//day17_inorder.c
 struct node {
     int data;
     struct node* left;
     struct node* right;
 };
+
+struct node* createNode(int value) {
+    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+struct node* insert(struct node* root, int value) {
+    if (root == NULL) {
+        return createNode(value);
+    }
+
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
