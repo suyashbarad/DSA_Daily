52dc8146665905b9a3a9be4cc14baef9b5cb39fa
suyashh <baradsuyash8@gmail.com>
2025-12-12 23:58:16 +0530

    Postorder Traversal (Rec)

--- day19_postorder.c
+++ day19_postorder.c
@@ -27,3 +27,28 @@ struct node* insert(struct node* root, int value) {
 
     return root;
 }
+void postorder_rec(struct node* root){
+    if(root != NULL){
+        postorder_rec(root->left);
+        postorder_rec(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main(){
+    struct node* root = NULL;
+    int n, value;
+
+    printf("Enter no. of elements: ");
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &value);
+        root = insert(root, value);
+    }
+    printf("Recursive postorder Traversal: ");
+    postorder_rec(root);
+    printf("\n");
+
+}
+
