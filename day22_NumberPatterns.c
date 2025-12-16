9b39f678b418962dc46148c4889b2e4ae5deb7a5
suyashh <baradsuyash8@gmail.com>
2025-12-16 23:16:57 +0530

    Number Triangle patterns

--- day22_NumberPatterns.c
+++ day22_NumberPatterns.c
@@ -4,7 +4,7 @@ int main(){
     int n = 5;
     int choice;
     while(1){
-        printf("Enter choice: ");
+        printf("1. Triangle\n2.InvertedTriangle\n3.Pyramid\n4.InvertedPyramid\n5.Triangle(sameNo)\n6.END\nEnter choice: ");
         scanf("%d", &choice);
         switch(choice){
             case 1:
@@ -15,7 +15,6 @@ int main(){
                 printf("\n");
             }
             break;
-            }
             case 2:
             for(int i = 1; i <= n; i++){
                 for(int j = i; j <= n; j++){
@@ -46,6 +45,18 @@ int main(){
                 printf("\n");
             }
             break;
+            case 5:
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= i; j++){
+                    printf("%d", i);
+                }
+                printf("\n");
+            }
+            break;
+            case 6:
+                printf("ThankYou!");
+                return 0;
+    }
 }   
     return 0;
     
