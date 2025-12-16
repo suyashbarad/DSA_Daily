5dff3e5d72b9320082f3dbe9c95b4e38801ffbf1
suyashh <baradsuyash8@gmail.com>
2025-12-16 23:16:06 +0530

    normal and inverted triangles

--- day22_NumberPatterns.c
+++ day22_NumberPatterns.c
@@ -23,7 +23,31 @@ int main(){
                 }
                 printf("\n");
             }
+            break;
+            case 3:
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= n-i; j++){
+                    printf(" ");
+                }
+                for(int j = 1; j <= 2*i - 1; j++){
+                    printf("%d", j);
+                }
+                printf("\n");
+            }
+            break;
+            case 4:
+            for(int i = n; i >= 1; i--){
+                for(int j = 1; j <= n-i; j++){
+                    printf(" ");
+                }
+                for(int j = 1; j <= 2*i - 1; j++){
+                    printf("%d", j);
+                }
+                printf("\n");
+            }
+            break;
 }   
     return 0;
     
 }
+
