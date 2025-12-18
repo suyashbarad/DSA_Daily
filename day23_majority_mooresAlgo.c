bb31d36ce574bb2dbf8d33e8bb95cda00a1dc49b
suyashh <baradsuyash8@gmail.com>
2025-12-18 15:30:27 +0530

    Moore's algo

--- day23_majority_mooresAlgo.c
+++ day23_majority_mooresAlgo.c
@@ -4,5 +4,17 @@ int main(){
     int count = 0, poss = 0;
     int n = 5;
     int arr[] = {2, 4, 6, 3, 2, 4, 2, 2, 3, 1};
-  return 0;
+    for(int i = 0; i < n; i++){
+        if(count == 0){
+            poss = arr[i];
+            count = 1;
+        }
+        else if(arr[i] == poss){
+            count++;
+        }
+        else{
+            count--;
+        }
+    }
+    printf("%d", poss);
 }
