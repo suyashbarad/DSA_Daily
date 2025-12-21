a94057a691cce136acf65ecbf216c38b50fe29ee
suyashh <baradsuyash8@gmail.com>
2025-12-22 00:23:55 +0530

    Checking Anagram condition

--- day28_anagram.c
+++ day28_anagram.c
@@ -3,8 +3,13 @@
 
 int main(){
 
-    char s1[] = "silent";
-    char s2[] = "listen";
+    char s1[100];
+    char s2[100];
+    printf("Enter First string: ");
+    scanf("%s", s1);
+    printf("Enter Second string: ");
+    scanf("%s", s2);
+    
     int flag = 0;
     int count[256] = {0};
     if(strlen(s1) != strlen(s2)){
