712580e420e4602cacef40368e81ad1195572916
suyashh <baradsuyash8@gmail.com>
2025-12-25 22:47:59 +0530

    Main code

--- day30_longest_Substring.c
+++ day30_longest_Substring.c
@@ -7,6 +7,19 @@ int main(){
     char ansStr[n+1];
     int count[255] = {0};
     int fcount = 0;
+    for(int i = 0; i < n; i++){
+        if(count[str[i]] == 0){
+            ansStr[i] = str[i];
+            count[str[i]]++;
+            printf("%c", ansStr[i]);
+            fcount++;
+        }
+        else{
+            // ansStr[n] = '\0';
+            break;
+        }
+    }
+    printf("\n%d", fcount);
+    
     return 0;
-
 }
