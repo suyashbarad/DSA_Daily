500dcfecd9caf223938f4d1221f6b10ddad1a701
suyashh <baradsuyash8@gmail.com>
2025-12-25 22:47:33 +0530

    necessory int's and char's

---
+++ day30_longest_Substring.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+
+    char str[] = "abcdaabs";
+    int n = strlen(str);
+    char ansStr[n+1];
+    int count[255] = {0};
+    int fcount = 0;
+    return 0;
+
+}
