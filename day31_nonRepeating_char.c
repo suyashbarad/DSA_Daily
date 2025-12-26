433c501fb4e5277e0d9b1f9f2013a7f294ac8786
suyashh <baradsuyash8@gmail.com>
2025-12-27 00:19:31 +0530

    required functions

---
+++ day31_nonRepeating_char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+    char str[] = "abccdd";
+    int n = strlen(str);
+    int count[256] = {0};
+    char ans = '\0';
+    
+    return 0;
+}
