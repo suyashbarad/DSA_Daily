a1b2711b394b6cf4fc13cf409089d8fd4971e934
suyashh <baradsuyash8@gmail.com>
2025-12-27 00:19:43 +0530

    Mian code

--- day31_nonRepeating_char.c
+++ day31_nonRepeating_char.c
@@ -7,6 +7,18 @@ int main(){
     int n = strlen(str);
     int count[256] = {0};
     char ans = '\0';
-    
+    for(int i = 0; i < n; i++){
+        count[str[i]]++;
+    }
+
+    for(int i = 0; i < n; i++){
+        if(count[str[i]] == 1){
+            ans = str[i];
+            break;
+        }
+    }
+    if(ans != '\0'){
+        printf("Ans: %c\n", ans);
+    }
     return 0;
 }
