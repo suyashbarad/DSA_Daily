15de2fff2bd7b243137a7944be1a0dd3ce801399
Suyash Sachin Barad <baradsuyash4@gmail.com>
2025-12-30 18:33:23 +0530

    day33

---
+++ day33_subarray_GivenSum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[] = {1, 4, 20, 3, 10, 5};
+    int S = 33;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int flag = 0;
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+        for(int j = i; j < n; j++){
+            sum += arr[j];
+            if(sum == S){
+                printf("Index: %d %d",i, j);
+                flag = 1;
+            }
+        }
+        if(flag) break;
+    }
+    if(!flag){
+        printf("not found");
+    }
+    return 0;
+}
