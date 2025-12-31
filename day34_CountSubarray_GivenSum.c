97e28ddaa82ba4ef1efc6b3b1d27c98d5ce67f0d
Suyash Sachin Barad <baradsuyash4@gmail.com>
2025-12-31 16:54:41 +0530

    day34

---
+++ day34_CountSubarray_GivenSum.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[] = {1, 1, 1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int req = 2;
+    // int flag = 0;
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        int sum = 0;
+        for(int j = i; j < n; j++){
+            sum += arr[j];
+            if(sum == req){
+                // flag = 1;
+                count++;
+            }
+        }
+    }
+    printf("Ans: %d\n", count);
+    return 0;
+}
