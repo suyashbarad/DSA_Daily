287a7a192fd325b52cd4b65602abea4115783240
suyashh <baradsuyash8@gmail.com>
2025-12-31 16:56:35 +0530

    Counts total subarrays with given sum

--- day34_CountSubarray_GivenSum.c
+++ day34_CountSubarray_GivenSum.c
@@ -2,15 +2,25 @@
 
 int main(){
 
-    int arr[] = {1, 1, 1};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {1, 1, 1};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter number at %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
     int req = 2;
+    // int flag = 0;
     int count = 0;
     for(int i = 0; i < n; i++){
         int sum = 0;
         for(int j = i; j < n; j++){
             sum += arr[j];
             if(sum == req){
+                // flag = 1;
                 count++;
             }
         }
