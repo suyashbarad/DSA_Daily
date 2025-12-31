059c0a229fab653382f17316b7e41b2e8de9c77a
suyashh <baradsuyash8@gmail.com>
2025-12-31 16:15:01 +0530

    Continuous subarray with given sum

--- day33_subarray_GivenSum.c
+++ day33_subarray_GivenSum.c
@@ -2,23 +2,34 @@
 
 int main(){
 
-    int arr[] = {1, 4, 20, 3, 10, 5};
+    // int arr[] = {1, 4, 20, 3, 10, 5};
+    // int n = sizeof(arr)/sizeof(arr[0]);  manual way
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter number at %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
     int S = 33;
-    int n = sizeof(arr)/sizeof(arr[0]);
     int flag = 0;
     for(int i = 0; i < n; i++){
         int sum = 0;
         for(int j = i; j < n; j++){
             sum += arr[j];
+
             if(sum == S){
-                printf("Found at Index: [%d, %d]",i, j);
+                printf("Index: %d %d",i, j);
                 flag = 1;
+                break;
             }
         }
         if(flag) break;
     }
+
     if(!flag){
-        printf("not found at any index");
+        printf("not found");
     }
     return 0;
 }
