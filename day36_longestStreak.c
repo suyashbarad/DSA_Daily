f0fa09fe8c8fae4609d8e57e2604544df9302fc9
Suyash Sachin Barad <baradsuyash4@gmail.com>
2026-01-02 02:05:27 +0530

    Longest streak of unsorted array

--- day36_longestStreak.c
+++ day36_longestStreak.c
@@ -2,8 +2,18 @@
 
 int main(){
     
-    int arr[] = {4, 2, 3, 5, 100, 101, 1};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {4, 2, 3, 5, 100, 101, 1};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
 
     for(int i = 0; i < n-1; i++){
         for(int j = 0; j < n-i-1; j++){
@@ -18,13 +28,15 @@ int main(){
     for(int i = 0; i < n; i++){
         printf("%d ", arr[i]);
     }
-    printf("\n");
+    printf("\nContinuous Streak: ");
 
     int sum = 0;
     int total = 0;
     int flag = 0;
-    for(int i = 0; i < n; i++){
+    printf("%d ", arr[0]);   //-->if streak broke from first elem, it'll still print first element
+    for(int i = 1; i < n; i++){
         if(arr[i] == arr[i-1]+1){
+            // printf("%d ", arr[0]);
             printf("%d ", arr[i]);
         }
         else{
