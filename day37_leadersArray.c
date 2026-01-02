a2e6cb23f265505510abf2771bb6559bc6240deb
suyashh <baradsuyash8@gmail.com>
2026-01-03 00:54:30 +0530

    Elements greater than right
    A leader is an element in the array such that no element to its right is greater than it.

--- day37_leadersArray.c
+++ day37_leadersArray.c
@@ -2,8 +2,20 @@
 
 int main(){
 
-    int arr[] = {16, 17, 4, 3, 5, 2};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {16, 17, 4, 3, 5, 2};
+    // int n = sizeof(arr)/sizeof(arr[0]);    -----> Manual way
+    
+    //  Leaders: the element which dont have no larger element on right that current...last element is always leader
+    
+    int n;
+    printf("Enter The number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter arr element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
 
     for(int i = 0; i < n; i++){
         int flag = 0;
