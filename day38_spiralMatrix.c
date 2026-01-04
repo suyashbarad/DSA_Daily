212a718a2bccb139518e5a8409b6c84aae4bb713
suyashh <baradsuyash8@gmail.com>
2026-01-04 22:41:05 +0530

    Printing matrix in spiral order

--- day38_spiralMatrix.c
+++ day38_spiralMatrix.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+//hardest
 void inputMatrix(int rows, int cols, int mat[rows][cols]){
     printf("Enter matrix elements: ");
     for(int i = 0; i < rows; i++){
@@ -15,4 +16,48 @@ void printMatrix(int rows, int cols, int mat[rows][cols]){
         }
         printf("\n");
     }
+}
+
+int main(){
+    int rows, cols;
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+    int n = rows*cols;
+    int mat[rows][cols];
+    // int flag[] = 0;
+    inputMatrix(rows, cols, mat);
+    printf("Matrix: \n");
+    printMatrix(rows, cols, mat);
+    printf("\nSpiral Form: ");
+    int top = 0, bottom = rows-1;
+    int left = 0, right = cols-1;
+
+    while(top <= bottom && left <= right){
+        // left->right
+        for(int j = left; j <= right; j++){
+            printf("%d ", mat[top][j]);
+        }
+        top++;
+        // top->bottom
+        for(int i = top; i <= bottom; i++){
+            printf("%d ", mat[i][right]);
+        }
+        right--;
+        // right->left
+        if(top <= bottom){
+            for(int j = right; j >= left; j--){
+                printf("%d ", mat[bottom][j]);
+            }
+            bottom--;
+        }
+        // bottom->top
+        if(left <= right){
+            for(int j = bottom; j >= top; j--){
+                printf("%d ", mat[j][left]);
+            }
+            left++;
+        }
+        
+    }
+    return 0;    
 }
