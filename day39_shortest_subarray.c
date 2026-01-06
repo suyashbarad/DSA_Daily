be35833fa2a0bed310d13ee93a09ecb0a15bf73e
suyashh <baradsuyash8@gmail.com>
2026-01-06 23:29:04 +0530

    trial

--- day39_shortest_subarray.c
+++ day39_shortest_subarray.c
@@ -2,8 +2,16 @@
 
 int main(){
 
-    int arr[] = {2, 3, 1, 2, 4, 3};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {2, 3, 1, 2, 4, 3};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter number at %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
     int k = 7;
     for(int i = 0; i < n; i++){
         int sum = 0;
