92e8ce8a0ef727c6390a2c95d1227af7a5114d68
Suyash Sachin Barad <baradsuyash4@gmail.com>
2026-01-11 15:25:50 +0530

    Main code

--- day42_largestNo.cpp
+++ day42_largestNo.cpp
@@ -2,8 +2,17 @@
 using namespace std;
 
 int main(){
-    int arr[] = {3, 2, 1, 5, 6, 4};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {3, 2, 1, 5, 6, 4};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    printf("Enter no. of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &arr[i]);
+    }
+    printf("\n");
     int largest = arr[0];
     for(int i = 1; i < n; i++){
         if(arr[i] > largest) largest = arr[i];
