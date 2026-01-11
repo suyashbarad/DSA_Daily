84633ff674f821e307da23490a4618ef72e79d86
suyashh <baradsuyash8@gmail.com>
2026-01-11 17:08:36 +0530

    Largest number -> C++

--- day42_largestNo.cpp
+++ day42_largestNo.cpp
@@ -2,9 +2,6 @@
 using namespace std;
 
 int main(){
-    // int arr[] = {3, 2, 1, 5, 6, 4};
-    // int n = sizeof(arr)/sizeof(arr[0]);
-    
     // int arr[] = {2, 7, 11, 5, 4};
     // int n = sizeof(arr)/sizeof(arr[0]);
     int n;
