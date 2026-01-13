dea881dd046bdd0df45bb21bbb5b20f8690f680b
Suyash Sachin Barad <baradsuyash4@gmail.com>
2026-01-14 01:01:21 +0530

    Day45

---
+++ day45_elemFreq.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // int arr[] = {1, 1, 2, 2, 2, 3, 3};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cout << "Enter no of elements: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cout << "Enter element " << i+1 << ": ";
+        cin >> arr[i];
+    }
+    int count[n];
+    int visited[n];
+    for(int i = 0; i < n; i++){
+        visited[i] = 0;
+        count[i] = 0;
+    }
+    for(int i = 0; i < n; i++){
+        if(visited[i] == 1)
+            continue;
+        count[i] = 1;
+        for(int j = i+1; j < n; j++){
+            if(arr[i] == arr[j]){
+                count[i]++;
+                visited[j] = 1;
+            }
+        }
+        cout << arr[i] << "->" << count[i] << endl;
+    }
+    return 0;
+}
