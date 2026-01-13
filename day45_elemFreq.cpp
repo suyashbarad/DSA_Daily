20aab0001c6ef6ae007e7d1bdb924dde59313f77
suyashh <baradsuyash8@gmail.com>
2026-01-14 01:06:07 +0530

    Counting element Frequency -> C++

--- day45_elemFreq.cpp
+++ day45_elemFreq.cpp
@@ -2,8 +2,16 @@
 using namespace std;
 
 int main(){
-    int arr[] = {1, 1, 2, 2, 2, 3, 3};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {1, 1, 2, 2, 2, 3, 3};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cout << "Enter no of elements: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cout << "Enter element " << i+1 << ": ";
+        cin >> arr[i];
+    }
     int count[n];
     int visited[n];
     for(int i = 0; i < n; i++){
