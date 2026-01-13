de1b2a2b2527b50cc9268b0bec8507679748ae47
Suyash Sachin Barad <baradsuyash4@gmail.com>
2026-01-13 19:01:21 +0530

    day44

---
+++ day44_reverseArr_str.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int choice;
+    int arr[] = {1, 2, 3, 4};
+    string str = "ABCDE";
+    
+    cout << "1.String, 2. Array: ";
+    cin>>choice;
+    switch(choice){
+        case 1:{
+            int n = str.length();
+            for(int i = 0; i < n/2; i++){
+                char temp = str[i];
+                str[i] = str[n-i-1];
+                str[n-i-1] = temp;
+            }
+            for(int i = 0; i < n; i++){
+                cout << str[i];
+            }
+            break;
+        }
+        case 2:{
+            int n1 = sizeof(arr)/sizeof(arr[0]);
+            for(int i = 0; i < n1/2; i++){
+                int temp = arr[i];
+                arr[i] = arr[n1-i-1];
+                arr[n1-i-1] = temp;
+            } 
+            for(int i = 0; i < n1; i++){ 
+                cout << arr[i];
+            }
+            break;
+        }
+    }
+    return 0;
+}
