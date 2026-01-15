4bb01f61235bcf62d3094da031bb3acb3ab8f137
Suyash Sachin Barad <baradsuyash4@gmail.com>
2026-01-15 17:16:14 +0530

    Day47

--- day47_secondLargest.cpp
+++ day47_secondLargest.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int main(){
-    int arr[] = {3, 4, 2, 1, 5, 6};
-    int n = sizeof(arr)/sizeof(arr[0]);
-
+    // int arr[] = {3, 4, 2, 1, 5, 6};
+    // int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+    cout << "Enter element no. " << i+1 << ": ";
+    cin >> arr[i];
+    }
     for(int i = 0; i < n-1; i++){
         for(int j = 0; j < n-i-1; j++){
             if(arr[j] > arr[j+1]){
